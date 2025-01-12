401903fcc8e769aec4660bbf3178071efd01d6c0
Henlorata <matuzabalazs@gmail.com>
2025-01-12 22:49:42 +0100

    Create new Card component Still needs correct data Needs expand fix

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {MyCard} from './MyCard'
+import {Post} from "./Post.jsx";
 
 
 export const CardsContainer = ({posts}) => {
     return (
         <div className='cardsContainer'>
-            {posts && posts.map(obj => <MyCard key={obj.id} {...obj}/>)}
+            {posts && posts.map(obj => <Post key={obj.id} {...obj}/>)}
         </div>
     )
 }
