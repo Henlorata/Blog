c8e0696d4c31eb4a2f12ed4d86a8155a655ff86e
Henlorata <matuzabalazs@gmail.com>
2025-01-13 03:41:08 +0100

    Fix Navbar profile showing nothing when logged out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,27 +12,26 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import {NavLink, Outlet} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "../context/UserContext.jsx";
-import {extractUrlAndId} from "../utility/utils.js";
-
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import { NavLink, Outlet } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext.jsx";
+import { extractUrlAndId } from "../utility/utils.js";
 
 export const Navbar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const {user, logoutUser} = useContext(UserContext)
-    const [avatar, setAvatar] = useState(null)
+    const { user, logoutUser } = useContext(UserContext);
+    const [avatar, setAvatar] = useState(null);
 
     useEffect(() => {
-        user?.photoURL && setAvatar(extractUrlAndId(user.photoURL).url)
-        !user && setAvatar(null)
-    }, [user, user?.photoURL])
+        user?.photoURL && setAvatar(extractUrlAndId(user.photoURL).url);
+        !user && setAvatar(null);
+    }, [user, user?.photoURL]);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
+
     const handleOpenUserMenu = (event) => {
         setAnchorElUser(event.currentTarget);
     };
@@ -50,7 +49,7 @@ export const Navbar = () => {
             <AppBar position="static">
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                        <AdbIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                         <Typography
                             variant="h6"
                             noWrap
@@ -58,7 +57,7 @@ export const Navbar = () => {
                             href="/"
                             sx={{
                                 mr: 2,
-                                display: {xs: 'none', md: 'flex'},
+                                display: { xs: 'none', md: 'flex' },
                                 fontFamily: 'monospace',
                                 fontWeight: 700,
                                 letterSpacing: '.3rem',
@@ -69,7 +68,7 @@ export const Navbar = () => {
                             BLOG
                         </Typography>
 
-                        <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                             <IconButton
                                 size="large"
                                 aria-label="account of current user"
@@ -78,7 +77,7 @@ export const Navbar = () => {
                                 onClick={handleOpenNavMenu}
                                 color="inherit"
                             >
-                                <MenuIcon/>
+                                <MenuIcon />
                             </IconButton>
                             <Menu
                                 id="menu-appbar"
@@ -94,61 +93,74 @@ export const Navbar = () => {
                                 }}
                                 open={Boolean(anchorElNav)}
                                 onClose={handleCloseNavMenu}
-                                sx={{display: {xs: 'block', md: 'none'}}}
+                                sx={{ display: { xs: 'block', md: 'none' } }}
                             >
-                                {<MenuItem key={'Posts'} onClick={handleCloseNavMenu}>
-                                    <Typography sx={{textAlign: 'center'}}>{'Pages'}</Typography>
-                                </MenuItem>}
-                                {<MenuItem key={'New Post'} onClick={handleCloseNavMenu}>
-                                    <Typography sx={{textAlign: 'center'}}>{'New Post'}</Typography>
-                                </MenuItem>}
+                                <MenuItem key={'Posts'} onClick={handleCloseNavMenu}>
+                                    <NavLink to="/posts" style={{ textDecoration: 'none', color: 'black' }}>
+                                        Posts
+                                    </NavLink>
+                                </MenuItem>
+                                {user && (
+                                    <MenuItem key={'New Post'} onClick={handleCloseNavMenu}>
+                                        <NavLink to="/create" style={{ textDecoration: 'none', color: 'black' }}>
+                                            New Post
+                                        </NavLink>
+                                    </MenuItem>
+                                )}
                             </Menu>
                         </Box>
-                        <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}}/>
-                        {/*<Typography*/}
-                        {/*    variant="h5"*/}
-                        {/*    noWrap*/}
-                        {/*    component="a"*/}
-                        {/*    href="#app-bar-with-responsive-menu"*/}
-                        {/*    sx={{*/}
-                        {/*        mr: 2,*/}
-                        {/*        display: {xs: 'flex', md: 'none'},*/}
-                        {/*        flexGrow: 1,*/}
-                        {/*        fontFamily: 'monospace',*/}
-                        {/*        fontWeight: 700,*/}
-                        {/*        letterSpacing: '.3rem',*/}
-                        {/*        color: 'inherit',*/}
-                        {/*        textDecoration: 'none',*/}
-                        {/*    }}*/}
-                        {/*>*/}
-                        {/*    LOGO*/}
-                        {/*</Typography>*/}
-                        <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                             <Button
                                 key={'Posts'}
                                 onClick={handleCloseNavMenu}
-                                sx={{my: 2, color: 'white', display: 'block'}}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
                                 href={'/posts'}
                             >
-                                {'Posts'}
+                                Posts
                             </Button>
-                            {user && <Button
-                                key={'New Post'}
-                                onClick={handleCloseNavMenu}
-                                sx={{my: 2, color: 'white', display: 'block'}}
-                                href={'/create'}
-                            >
-                                {'New Post'}
-                            </Button>}
+                            {user && (
+                                <Button
+                                    key={'New Post'}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                    href={'/create'}
+                                >
+                                    New Post
+                                </Button>
+                            )}
                         </Box>
-                        <Box sx={{flexGrow: 0}}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
-                                </IconButton>
-                            </Tooltip>
+
+                        <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
+                            {!user && (
+                                <Button
+                                    key={'Register'}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', marginRight: 2 }}
+                                    href={'/auth/up'}
+                                >
+                                    Register
+                                </Button>
+                            )}
+                            {!user && (
+                                <Button
+                                    key={'Login'}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white' }}
+                                    href={'/auth/in'}
+                                >
+                                    Login
+                                </Button>
+                            )}
+                            {user && (
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="User Avatar" src={avatar || '/static/images/avatar/2.jpg'} />
+                                    </IconButton>
+                                </Tooltip>
+                            )}
                             <Menu
-                                sx={{mt: '45px'}}
+                                sx={{ mt: '45px' }}
                                 id="menu-appbar"
                                 anchorEl={anchorElUser}
                                 anchorOrigin={{
@@ -163,27 +175,33 @@ export const Navbar = () => {
                                 open={Boolean(anchorElUser)}
                                 onClose={handleCloseUserMenu}
                             >
-                                {user ?
+                                {user && (
                                     <>
                                         <MenuItem key={'Profile'} onClick={handleCloseUserMenu}>
-                                            <NavLink to={'/profile'} style={{textDecoration: 'none', color: 'black', textAlign: 'center'}}>Profile</NavLink>
-                                        </MenuItem>
-                                        <MenuItem key={'Account'} onClick={handleCloseUserMenu}>
-                                            <NavLink style={{textDecoration: 'none', color: 'black', textAlign: 'center'}}>Account</NavLink>
+                                            <NavLink to={'/profile'} style={{
+                                                textDecoration: 'none',
+                                                color: 'black',
+                                                textAlign: 'center',
+                                            }}>Profile</NavLink>
                                         </MenuItem>
                                         <MenuItem key={'Logout'} onClick={() => {
                                             handleCloseUserMenu();
-                                            logoutUser()
+                                            logoutUser();
                                         }}>
-                                            <NavLink to={'/'} style={{textDecoration: 'none', color: 'black', textAlign: 'center'}}>Logout</NavLink>
+                                            <NavLink to={'/'} style={{
+                                                textDecoration: 'none',
+                                                color: 'black',
+                                                textAlign: 'center',
+                                            }}>Logout</NavLink>
                                         </MenuItem>
-                                    </> : <></>}
+                                    </>
+                                )}
                             </Menu>
                         </Box>
                     </Toolbar>
                 </Container>
             </AppBar>
-            <Outlet/>
+            <Outlet />
         </div>
-);
-}
+    );
+};
